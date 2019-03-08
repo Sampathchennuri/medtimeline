a77ac8f44febcb1a5d4253ba1acf9a079a11cde5
laurendukes <laurendukes@google.com>
2019-03-08 16:21:04 -0500

    General code cleanups.
    Change-Id: Ic3fd4e098d5725b09b0b53e33dd274e52baa97cf
    GitOrigin-RevId: de855de78fb38e05c92d19476dbe4168a9ae3071

--- medtimeline/src/app/graphtypes/tooltips/observation-tooltips.ts
+++ medtimeline/src/app/graphtypes/tooltips/observation-tooltips.ts
@@ -4,8 +4,7 @@
 // license that can be found in the LICENSE file.
 
 import {DomSanitizer} from '@angular/platform-browser';
-import * as Color from 'color';
-import {DateTime} from 'luxon';
+import {Color} from 'd3';
 import {AnnotatedObservation} from 'src/app/fhir-data-classes/annotated-observation';
 
 import {Observation} from '../../fhir-data-classes/observation';
@@ -47,6 +46,9 @@ export class GenericAnnotatedObservationTooltip extends
    */
   getTooltip(observation: AnnotatedObservation, sanitizer: DomSanitizer): string
       |undefined {
+    if (observation.annotationValues.length === 0) {
+      return undefined;
+    }
     const table = Tooltip.createNewTable();
     if (this.addTimestampRow) {
       Tooltip.addTimeHeader(
@@ -64,40 +66,6 @@ export class GenericAnnotatedObservationTooltip extends
     for (const annotation of observation.annotationValues) {
       Tooltip.addRow(table, annotation, sanitizer);
     }
-    return table.outerHTML;
-  }
-}
-
-
-/**
- * Makes a generic tooltip for an AnnotatedObservation with rows for each of
- * its annotation values.
- */
-export class GenericAbnormalTooltip extends
-    Tooltip<{[key: string]: number | string}> {
-  constructor(private addTimestampRow: boolean, private color: Color) {
-    super();
-  }
-
-  /**
-   * Returns the HTML for a generic tooltip.
-   * @param params The parameters necessary for the AbnormalTooltip text. It
-   *     should contain a timestamp field, a value field, a label field, and a
-   *     unit field.
-   * @param sanitizer A DOM sanitizer
-   * @returns If the observation has annotations, a HTML table with the
-   *     annotation values. If there are no annotations, will return undefined.
-   */
-  getTooltip(params: {[key: string]: number|string}, sanitizer: DomSanitizer):
-      string|undefined {
-    const table = Tooltip.createNewTable();
-    const millis: any = params['timestamp'];
-    const timestamp = DateTime.fromMillis(millis);
-    if (this.addTimestampRow) {
-      Tooltip.addTimeHeader(timestamp, table, sanitizer);
-    }
-
-    Tooltip.addHeader('Caution: value outside normal range', table, sanitizer);
     return table.outerHTML;
   }
 }
