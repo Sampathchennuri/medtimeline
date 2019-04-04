bcbf1ef5d738d7b2aebe61969b3c4c316b779ce5
laurendukes <laurendukes@google.com>
2019-04-04 18:14:30 -0400

    Merge branch 'master' into opensource
    Change-Id: Id3cd41dcf205b7bb0c3e3c4788e39dae2d2dccca
    GitOrigin-RevId: cff57f51f0a98376c38992a2824939e4f0ee4403

--- medtimeline/src/app/graphtypes/stepgraph/stepgraph.component.ts
+++ medtimeline/src/app/graphtypes/stepgraph/stepgraph.component.ts
@@ -8,56 +8,30 @@ import {DomSanitizer} from '@angular/platform-browser';
 import * as Color from 'color';
 import {DateTime} from 'luxon';
 import {MicrobioGraphData} from 'src/app/graphdatatypes/microbiographdata';
-import {getDataColors} from 'src/app/theme/bch_colors';
 
 import {StepGraphData} from '../../graphdatatypes/stepgraphdata';
 import {GraphComponent} from '../graph/graph.component';
+import {RenderedChart} from '../graph/renderedchart';
 
 @Component({
   selector: 'app-stepgraph',
   templateUrl: '../graph/graph.component.html',
-  // TODO(b/117575935): These styles need to be extracted and generalized
   styleUrls: ['../graph.css'],
   providers: [
     {provide: GraphComponent, useExisting: forwardRef(() => StepGraphComponent)}
   ]
 })
-
 export class StepGraphComponent extends
     GraphComponent<StepGraphData|MicrobioGraphData> {
   types: {[key: string]: string} = {};
   constructor(readonly sanitizer: DomSanitizer) {
-    super(sanitizer);
-  }
-
-  /**
-   * @override
-   */
-  generateChart() {
-    this.adjustYAxisConfig();
-    this.generateBasicChart();
-    this.adjustDataDependent();
-
-    if (!this.noDataPointsInDateRange) {
-      this.chartConfiguration.grid.y = {show: true};
-    }
+    super(sanitizer, (axis, id) => new RenderedChart(axis, id));
   }
 
   /**
    * Adjusts the y-axis configuration for the chart.
    */
-  adjustYAxisConfig() {
-    // Give labels to each series and make a map of
-    // x-values to y-values.
-    // Populate the stepgraphcard with data according to c3 format.
-    for (const series of this.data.series) {
-      const label = series.label;
-      if (series.concept) {
-        this.colorsMap[label] = series.concept.fill;
-      } else {
-        this.colorsMap[label] = getDataColors()[0];
-      }
-    }
+  prepareForChartConfiguration() {
     for (const endpointSeries of this.data.endpointSeries) {
       const endpointSeriesId = endpointSeries.label;
       this.data.c3DisplayConfiguration.allColumns.push(
@@ -72,50 +46,19 @@ export class StepGraphComponent extends
       this.colorsMap[endpointSeriesId] = Color.rgb(0, 0, 0);
     }
 
-    // The y-axis configuration for this chart maps each tick on the y-axis,
-    // initially numbers, to discrete labels representing each Medication's
-    // label, and sets the tick values to be those labels.
-    const yValues = Array.from(this.data.yAxisMap.keys());
-    // We need a slightly larger padding for step charts to be aligned with all
-    // other charts.
-    const stepGraphYAxisTickMax = 15;
-    this.yAxisTickDisplayValues =
-        yValues.map(value => this.data.yAxisMap.get(value));
-    if (this.data.yAxisMap.size === 0) {
-      this.data.yAxisMap.set(10, '');
-    }
-
-    this.yAxisConfig = {
-      // We add the min and max so that when series are hidden by being clicked
-      // on, the y axis does not change and the hidden medications' tick marks
-      // still appear
-      min: 10,
-      max: yValues.sort()[yValues.length - 1],
-      padding: {top: 20, bottom: 20},
-      tick: {
-        // We add padding to our y-axis tick labels so that all y-axes of the
-        // charts rendered on the page can be aligned.
-        // We use an empty string placeholder for each label, so that the axis
-        // does not get shifted over.
-        format: d => {
-          return ''.trim().padStart(stepGraphYAxisTickMax, '\xa0');
-        },
-        values: Array.from(this.data.yAxisMap.keys()),
+    this.yAxisConfig.tick = {
+      // We add padding to our y-axis tick labels so that all y-axes of the
+      // charts rendered on the page can be aligned.
+      // We use an empty string placeholder for each label, so that the axis
+      // does not get shifted over.
+      format: d => {
+        return this.data.yAxisMap.get(d);
       },
+      values: Array.from(this.data.yAxisMap.keys()),
     };
   }
 
-  /**
-   * Adjusts the data-dependent fields of the chart's configuration.
-   */
-  adjustDataDependent() {
-    // Check if there are any data points in the time range.
-    this.noDataPointsInDateRange =
-        !GraphComponent.dataPointsInRange(this.data.series, this.dateRange) &&
-        !GraphComponent.dataPointsInRange(
-            this.data.endpointSeries, this.dateRange);
-
-    // Don't draw lines between endpoints.
-    this.chartConfiguration.data.types = this.types;
+  adjustGeneratedChartConfiguration() {
+    this.chartConfiguration.grid.y = {show: true};
   }
 }
